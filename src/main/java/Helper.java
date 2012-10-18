4107b7bbf53b20527df5f074938a1fadb42a4e83
bcornies <Bryan.Cornies@sjrb.ca>
2012-10-18 12:12:31 -0600

    Add basic program layout

--- src/main/java/Helper.java
+++
@@ -1,9 +0,0 @@
-public class Helper {
-
-
-    public static void main(String [ ] args) {
-        System.out.print("first!!!");
-        System.out.print("second!!!");
-    }
-
-}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String [ ] args) {
-        System.out.print("Thread234!");
+        List<Shape> shapes = new ArrayList<Shape>();
+        System.out.println("Drawing shapes...");
+        for (Shape shape : shapes) {
+            System.out.println(shape.draw());
+        }
     }
 }
